80c5f204cc6faa15296d4dadbeed779ce01ea754
johnzylstra <johnmartinzylstra@gmail.com>
2021-03-08 15:32:10 -0500

    ann and cnn for mnist digits

--- lab2.py
+++ lab2.py
@@ -14,12 +14,12 @@ tf.random.set_seed(1618)
 #tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-ALGORITHM = "guesser"
+#ALGORITHM = "guesser"
 #ALGORITHM = "tf_net"
-#ALGORITHM = "tf_conv"
+ALGORITHM = "tf_conv"
 
-DATASET = "mnist_d"
-#DATASET = "mnist_f"
+#DATASET = "mnist_d"
+DATASET = "mnist_f"
 #DATASET = "cifar_10"
 #DATASET = "cifar_100_f"
 #DATASET = "cifar_100_c"
@@ -56,13 +56,41 @@ def guesserClassifier(xTest):
 
 
 def buildTFNeuralNet(x, y, eps = 6):
-    pass        #TODO: Implement a standard ANN here.
-    return None
+    model = keras.Sequential()
+    lossType = keras.losses.categorical_crossentropy
+    model.add(keras.layers.Dense(512, activation = "relu"))
+    model.add(keras.layers.Dropout(0.2))
+    model.add(keras.layers.Dense(10, activation = "softmax"))
+    model.compile(optimizer = "adam", loss = lossType)
+    model.fit(x,y,epochs = eps)
+    return model
 
 
 def buildTFConvNet(x, y, eps = 10, dropout = True, dropRate = 0.2):
-    pass        #TODO: Implement a CNN here. dropout option is required.
-    return None
+    model = keras.Sequential();
+    inShape = (IH, IW, IZ)
+    lossType = keras.losses.categorical_crossentropy;
+
+    model.add(keras.layers.Conv2D(32, kernel_size = (3,3), activation="relu", padding="same", input_shape=inShape))
+    model.add(keras.layers.Conv2D(32, kernel_size = (3,3), activation="relu", padding="same"))
+    model.add(keras.layers.MaxPooling2D(pool_size = (2,2)))
+    model.add(keras.layers.Conv2D(64, kernel_size = (3,3), activation="relu", padding="same", input_shape=inShape))
+    model.add(keras.layers.Conv2D(64, kernel_size = (3,3), activation="relu", padding="same"))
+    model.add(keras.layers.MaxPooling2D(pool_size = (2,2)))
+    model.add(keras.layers.Conv2D(128, kernel_size = (3,3), activation="relu", padding="same", input_shape=inShape))
+    model.add(keras.layers.Conv2D(128, kernel_size = (3,3), activation="relu", padding="same", input_shape=inShape))
+    model.add(keras.layers.MaxPooling2D(pool_size = (2,2)))
+
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(200, activation = "relu"))
+    model.add(keras.layers.Dropout(dropRate))
+    model.add(keras.layers.Dense(200, activation = "relu"))
+    model.add(keras.layers.Dropout(dropRate))
+    model.add(keras.layers.Dense(NUM_CLASSES, activation="softmax"))
+
+    model.compile(optimizer="adam", loss=lossType)
+    model.fit(x,y,epochs=20)
+    return model
 
 #=========================<Pipeline Functions>==================================
 
